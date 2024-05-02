af1c20074f4ffaba89584ccb458fcf3d0a6343aa
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2024-05-02 17:00:57 +0300

    Added Player.cs and input action with constant class to saved them in the helper class

---
+++ Scripts/Helper/InputActionNames.cs
@@ -0,0 +1,32 @@
+namespace YourFirst3DGame.Scripts.Helper;
+
+/// <summary>
+/// Contains references to the Input map actions names
+/// </summary>
+public class InputActionNames
+{
+    /// <summary>
+    /// Reference to the<c>move_left</c>action name
+    /// </summary>
+    public const string MOVE_LEFT = "move_left";
+
+    /// <summary>
+    /// Reference to the<c>move_right</c>action name
+    /// </summary>
+    public const string MOVE_RIGHT = "move_right";
+
+    /// <summary>
+    /// Reference to the<c>move_forward</c>action name
+    /// </summary>
+    public const string MOVE_FORWARD = "move_forward";
+
+    /// <summary>
+    /// Reference to the<c>move_backward</c>action name
+    /// </summary>
+    public const string MOVE_BACKWARD = "move_backward";
+
+    /// <summary>
+    /// Reference to the<c>jump</c>action name
+    /// </summary>
+    public const string JUMP = "jump";
+}
